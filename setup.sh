8a83d5111ba01a2736954159c40b2cb76682e0e5
reata <reddevil.hjw@gmail.com>
2023-05-02 13:22:09 +0800

    feat: install docker engine

--- setup.sh
+++ setup.sh
@@ -3,25 +3,40 @@ set -e
 
 # packages needed for the install process
 sudo apt update
-sudo apt -y install curl apt-transport-https lsb-release gnupg
+sudo apt -y install ca-certificates curl apt-transport-https lsb-release gnupg
 
-# add sbt repository and signing key
+# set up repository and signing key
+## sbt
 echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
 echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
 curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" \
  | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sbt.gpg > /dev/null
+## docker
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # set up apt mirror
 sudo sed -i "s@http://.*archive.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
 sudo sed -i "s@http://.*security.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
 
-# update repository information and install our packages
+# update repository information
 sudo apt update
-sudo apt -y install default-jdk git maven nodejs npm python3 sbt scala ssh
 
-# install packages for daily development
-sudo apt -y install vim
+# install our packages
+sudo apt -y install default-jdk git maven nodejs npm python3 sbt scala ssh vim
 
 # install packages for Python build from source, which is needed for pyenv
 sudo apt -y install build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev python-tk tk-dev
+
+# install docker engine
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# post-installation steps for docker engine
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
